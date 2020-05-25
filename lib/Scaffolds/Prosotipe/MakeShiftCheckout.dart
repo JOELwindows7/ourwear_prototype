14027fa9136ac4493cda2ab1c241573a865134e6
Joel Robert Justiawan <joelwindows7@gmail.com>
2020-05-25 15:37:32 +0700

    Create MakeShiftCheckout.dart
    uhhh

---
+++ lib/Scaffolds/Prosotipe/MakeShiftCheckout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MakeShiftCheckOut extends StatefulWidget {
+  @override
+  _MakeShiftCheckOutState createState() => _MakeShiftCheckOutState();
+}
+
+class _MakeShiftCheckOutState extends State<MakeShiftCheckOut> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CheckOut'),
+      ),
+    );
+  }
+}
